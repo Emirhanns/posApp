e7de7b8bda720b40da91b8928c769ebd0e46f524
Emirhanns <ctn.emrhn0@gmail.com>
2024-02-05 22:47:03 +0300

    add crud process

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -3,6 +3,15 @@ const Category = require("../models/CategoryModel.js");
 const express = require("express");
 const router = express.Router();
 
+router.get("/get-all",async (req,res)=> {
+    try{
+        const categories = await Category.find();
+        res.status(200).json(categories)
+    }catch (error){
+        console.log(error);
+    }
+})
+
 router.post("/add-category", async (req, res) => {
     try {
         const newCategory = new Category(req.body);
@@ -12,5 +21,26 @@ router.post("/add-category", async (req, res) => {
         res.status(400).json(error)
     }
 })
+
+router.put("/update-category",async (req,res)=> {
+    try{
+        await Category.findOneAndUpdate({_id:req.body.categoryId},req.body);
+        res.status(200).json("Güncelleme Başarılı")
+    }catch (error){
+        console.log(error);
+    }
+})
+
+router.delete("/delete-category",async (req,res)=> {
+    try{
+        await Category.findOneAndDelete({_id:req.body.categoryId});
+        res.status(200).json("Silme Başarılı")
+    }catch (error){
+        console.log(error);
+    }
+})
+
+
+
 
 module.exports = router;
