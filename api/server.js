f56fb1b43744a722b3a4fd4f3aec0e3f266380f7
Emirhanns <ctn.emrhn0@gmail.com>
2024-02-10 21:03:49 +0300

    client and api integration

--- api/server.js
+++ api/server.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const app = express();
 const cors = require("cors");
 const port = 5000;
+const app = express();
+
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDb ye bağlandı");
+    } catch (error) {
+        throw error;
+    }
+};
+
+// CORS middleware'ini rotalardan önce tanımla
+app.use(cors());
 
 //routes
-
 const CategoryRoute = require("./routes/categories.js")
 const ProductRoute = require("./routes/products.js")
 const BillRoute = require("./routes/bills.js")
 const AuthRoute = require("./routes/auth.js")
 const UserRoute = require("./routes/users.js")
-
-
-
-dotenv.config();
-
-const connect = async () => {
-    try{
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log("MongoDb ye bağlandı");
-}catch (error){
-            throw error;
-    }
-};
 
 //middlewares
 app.use(express.json());
@@ -33,15 +33,6 @@ app.use("/api/products",ProductRoute);
 app.use("/api/bills",BillRoute);
 app.use("/api/auth",AuthRoute);
 app.use("/api/users",UserRoute);
-
-
-
-
-app.use(cors())
-
-
-
-
 
 app.listen(port, () => {
     console.log(`Örnek ${port}`);
--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,46 +1,89 @@
-export const Categories = () => {
-  return (
-    
-        <ul className='flex gap-2 md:flex-col overflow-y-auto text-md grid-cols-3' style={{ maxHeight: '75vh',paddingInline: '7px' }}>
+import "./style.css";
+import { PlusOutlined } from "@ant-design/icons";
+import { Modal, Form, Input, Button, message } from "antd";
+import { useState } from "react";
 
-            <li className='bg-green-700 px-2 py-4 text-white  cursor-pointer hover:bg-blue-400 transition-all text-center'>
+const Categories = () => {
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+    const [form] = Form.useForm()
+
+
+    const onFinish = (values)=>{
+        try{
+            fetch("http://localhost:5000/api/categories/add-category",{
+                method:"POST",
+                body: JSON.stringify(values),
+                headers: {"Content-type":"application/json; charset=UTF-8"},
+            });
+            message.success("Kategori Eklendi");
+            form.resetFields();
+            
+            ;
+        }catch (error){
+            console.log(error.message);
+
+        }
+    }
+
+    return (
+
+        <ul className='flex gap-2 md:flex-col overflow-y-auto text-md grid-cols-3' style={{ maxHeight: '75vh', paddingInline: '7px' }}>
+
+            <li className='category-item'>
                 <span>Tümü</span>
             </li>
 
-            <li className='bg-green-700 px-2 py-4 text-white  cursor-pointer hover:bg-blue-400 transition-all text-center'>
+            <li className='category-item'>
                 <span>Yiyecek</span>
             </li>
 
-            <li className='bg-green-700 px-2 py-4 text-white  cursor-pointer hover:bg-blue-400 transition-all text-center'>
+            <li className='category-item'>
                 <span>İçecek</span>
             </li>
 
-            <li className='bg-green-700 px-2 py-4 text-white  cursor-pointer hover:bg-blue-400 transition-all text-center'>
+            <li className='category-item'>
                 <span>Meyve</span>
             </li>
 
-            <li className='bg-green-700 px-2 py-4 text-white  cursor-pointer hover:bg-blue-400 transition-all text-center'>
+            <li className='category-item'>
                 <span>Sebze</span>
             </li>
 
-            <li className='bg-green-700 px-2 py-4 text-white  cursor-pointer hover:bg-blue-400 transition-all text-center'>
+            <li className='category-item'>
                 <span>Sebze</span>
             </li>
 
-            <li className='bg-green-700 px-2 py-4 text-white  cursor-pointer hover:bg-blue-400 transition-all text-center'>
+            <li className='category-item'>
                 <span>Sebze</span>
             </li>
 
 
-            <li className='bg-green-700 px-2 py-4 text-white  cursor-pointer hover:bg-blue-400 transition-all text-center'>
-                <span>Sebze en alt</span>
+            <li className='category-item !bg-purple-800 hover:opacity-70' onClick={() => setIsAddModalOpen(true)}>
+                <PlusOutlined className="md:text-2xl" />
             </li>
+            <Modal title="Yeni Kategori Ekle" open={isAddModalOpen} onCancel={() => setIsAddModalOpen(false)} footer={false} >
+                <Form layout="vertical" onFinish={onFinish} form ={form} >
+
+                    <Form.Item name={"title"} label="Kategori Ekle" 
+                    rules={[{required:true, message:"Kategori Boş Geçilemez"}]} >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item className="flex justify-end mb-0">
+                        <Button htmlType="submit">Oluştur</Button>
+                    </Form.Item>
+
+                </Form>
+
+            </Modal>
+
+
+
 
         </ul>
 
 
-    
-  )
+
+    )
 }
 
 export default Categories
